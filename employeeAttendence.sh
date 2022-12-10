685aae55adfe200aace3f5cff8b7dbc5c5f1807f
vssandip <sandipshindesr@gmail.com>
2022-12-10 12:06:40 +0530

    Added:  employeeAttendence.sh added

---
+++ employeeAttendence.sh
@@ -0,0 +1,12 @@
+isPresent=$((RANDOM%3));
+perHourSalary=20;
+workingHour=0;
+if [ $isPresent -eq 0 ]
+then
+	echo "Employee is absent";
+	workingHour=0;
+elif [ $isPresent -eq 1 ]
+then
+	echo "Employee is present";
+	workingHour=8;
+fi
