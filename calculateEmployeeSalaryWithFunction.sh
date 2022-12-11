d88c344f424c019333b77e13dc7f47e08432237c
vssandip <sandipshindesr@gmail.com>
2022-12-11 11:48:27 +0530

    Added: calculateEmployeeSalaryWithFunction.sh add

---
+++ calculateEmployeeSalaryWithFunction.sh
@@ -0,0 +1,36 @@
+function calculateWorkingHour() {
+	case $1 in
+		0)
+			workingHour=0;
+			;;
+
+		1)
+			workingHour=8;
+			;;
+		2)
+			workingHour=4;
+			;;
+	esac;
+	echo $workingHour;
+}
+
+perHourSalary=20;
+totalSalary=0;
+totalWorkingHour=0;
+day=1;
+
+while [[ $day -le 20 && $totalWorkingHour -lt 100 ]]
+do
+	wHour=$(calculateWorkingHour $((RANDOM%3)));
+	totalWorkingHour=$(($totalWorkingHour + $wHour));
+	if [ $totalWorkingHour -gt 100 ]
+	then
+		totalWorkingHour=$(($totalWorkingHour - $wHour));
+		break;
+	fi
+	salary=$(($perHourSalary * $wHour));
+	totalSalary=$(($totalSalary + $salary));
+	((day++));
+done
+echo "Employee has earned $totalSalary $ in a month (Total working Hour : $totalWorkingHour)";
+
